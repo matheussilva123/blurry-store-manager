041012ddce70c14f6713f310d943340e57cab0fb
matheus.dias <matheus.dias@ifood.com.br>
2022-02-20 13:56:48 -0300

    feat: initialize project

---
+++ blurry-store-manager-api/src/main/java/br/com/dias/blurrystoremanager/BlurryStoreManagerApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.dias.blurrystoremanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlurryStoreManagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlurryStoreManagerApiApplication.class, args);
+    }
+
+}
---
+++ blurry-store-manager-worker/src/main/java/br/com/dias/blurrystoremanager/BlurryStoreManagerWorkerApplication.java
@@ -0,0 +1,13 @@
+package br.com.dias.blurrystoremanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlurryStoreManagerWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlurryStoreManagerWorkerApplication.class, args);
+    }
+
+}
